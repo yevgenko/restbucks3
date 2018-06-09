aa5b121c886aa00676eb214fe9f4a0d04d765fab
Yevhen V <wik@bdhr.co>
2018-06-10 00:28:18 +0100

    Introduce workflow activity classes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   has_one :payment
 
+  # Could use concerns to inject activities without touching models,
+  # perhaps even with special DSL describing describing Domain Application Protocol (DAP)
   after_create :create_order_activity
 
   def preparing?
@@ -10,7 +12,15 @@ class Order < ApplicationRecord
   private
 
   def create_order_activity
-    create_payment
-    update_attribute :status, 'payment-expected'
+    CreateOrderActivity.perform(self)
+  end
+
+  class CreateOrderActivity
+    class << self
+      def perform(order)
+        order.create_payment
+        order.update_attribute :status, 'payment-expected'
+      end
+    end
   end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -6,9 +6,17 @@ class Payment < ApplicationRecord
   private
 
   def update_payment_activity
-    # TODO: possibly process payment through 3rd party service
-    # TODO: record payment date
-    # TODO: validate order status, i.e. "payment-expected" only!
-    order.update_attribute :status, 'preparing'
+    UpdatePaymentActivity.perform(self)
+  end
+
+  class UpdatePaymentActivity
+    class << self
+      def perform(payment)
+        # TODO: possibly process payment through 3rd party service
+        # TODO: record payment date
+        # TODO: validate order status, i.e. "payment-expected" only!
+        payment.order.update_attribute :status, 'preparing'
+      end
+    end
   end
 end
